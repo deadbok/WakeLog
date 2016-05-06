57cedf735b9eeb0fcdcc18ff580dc6e185c29b65
Martin Grønholdt <martin.groenholdt@gmail.com>
2016-05-07 00:26:17 +0200

    Settings UI first version.

--- README.md
+++ README.md
@@ -5,5 +5,10 @@ Simple Android app to read the log of a WakeLog device.
 
 *This is my very first Android program, and in no way a reference for perfect Android coding.*
 
+Devices view.
+-------------
 ![Devices view](https://raw.githubusercontent.com/deadbok/WakeLog/master/docs/devices.png)
+
+Log view.
+---------
 ![Log view](https://raw.githubusercontent.com/deadbok/WakeLog/master/docs/log.png)
--- app/src/main/java/net/groenholdt/wakelog/SettingsActivity.java
+++ app/src/main/java/net/groenholdt/wakelog/SettingsActivity.java
@@ -4,24 +4,23 @@ package net.groenholdt.wakelog;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.ListPreference;
+import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
-import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuItem;
 
 import java.util.List;
+
+//TODO Default values.
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
@@ -36,71 +35,13 @@ import java.util.List;
  */
 public class SettingsActivity extends AppCompatPreferenceActivity
 {
+    private static final String TAG = "SettingsActivity";
     /**
      * A preference value change listener that updates the preference's summary
      * to reflect its new value.
      */
-    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener =
-            new Preference.OnPreferenceChangeListener()
-            {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object value)
-                {
-                    String stringValue = value.toString();
-
-                    if (preference instanceof ListPreference)
-                    {
-                        // For list preferences, look up the correct display value in
-                        // the preference's 'entries' list.
-                        ListPreference listPreference = (ListPreference) preference;
-                        int index = listPreference.findIndexOfValue(stringValue);
-
-                        // Set the summary to reflect the new value.
-                        preference.setSummary(
-                                index >= 0
-                                ? listPreference.getEntries()[index]
-                                : null);
-
-                    }
-                    else if (preference instanceof RingtonePreference)
-                    {
-                        // For ringtone preferences, look up the correct display value
-                        // using RingtoneManager.
-                        if (TextUtils.isEmpty(stringValue))
-                        {
-                            // Empty values correspond to 'silent' (no ringtone).
-                            preference.setSummary(R.string.pref_ringtone_silent);
-
-                        }
-                        else
-                        {
-                            Ringtone ringtone = RingtoneManager.getRingtone(
-                                    preference.getContext(), Uri.parse(stringValue));
-
-                            if (ringtone == null)
-                            {
-                                // Clear the summary if there was a lookup error.
-                                preference.setSummary(null);
-                            }
-                            else
-                            {
-                                // Set the summary to reflect the new ringtone display
-                                // name.
-                                String name = ringtone.getTitle(preference.getContext());
-                                preference.setSummary(name);
-                            }
-                        }
-
-                    }
-                    else
-                    {
-                        // For all other preferences, set the summary to the value's
-                        // simple string representation.
-                        preference.setSummary(stringValue);
-                    }
-                    return true;
-                }
-            };
+    private static Preference.OnPreferenceChangeListener listener;
+    private static SharedPreferences prefs;
 
     /**
      * Helper method to determine if the device has an extra-large screen. For
@@ -112,37 +53,38 @@ public class SettingsActivity extends AppCompatPreferenceActivity
                 & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
     }
 
-    /**
-     * Binds a preference's summary to its value. More specifically, when the
-     * preference's value is changed, its summary (line of text below the
-     * preference title) is updated to reflect the value. The summary is also
-     * immediately updated upon calling this method. The exact display format is
-     * dependent on the type of preference.
-     *
-     * @see #sBindPreferenceSummaryToValueListener
-     */
-    private static void bindPreferenceSummaryToValue(Preference preference)
-    {
-        // Set the listener to watch for value changes.
-        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
-
-        // Trigger the listener immediately with the preference's
-        // current value.
-        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
-                                                                 PreferenceManager
-                                                                         .getDefaultSharedPreferences(
-                                                                                 preference
-                                                                                         .getContext())
-                                                                         .getString(preference
-                                                                                            .getKey(),
-                                                                                    ""));
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setupActionBar();
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        listener = new Preference.OnPreferenceChangeListener()
+        {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object value)
+            {
+                String stringValue = value.toString();
+
+                Log.d(TAG, "Preference " + preference.getKey() + "changed.");
+
+                if (preference instanceof EditTextPreference)
+                {
+                    // Set the summary to reflect the new value.
+                    preference.setSummary(String.format(
+                            getString(R.string.pref_title_timeout_value), stringValue));
+                }
+                else
+                {
+                    // For all other preferences, set the summary to the value's
+                    // simple string representation.
+                    preference.setSummary(stringValue);
+                }
+                return true;
+            }
+        };
     }
 
     /**
@@ -199,9 +141,8 @@ public class SettingsActivity extends AppCompatPreferenceActivity
     protected boolean isValidFragment(String fragmentName)
     {
         return PreferenceFragment.class.getName().equals(fragmentName)
-               || GeneralPreferenceFragment.class.getName().equals(fragmentName)
-               || DataSyncPreferenceFragment.class.getName().equals(fragmentName)
-               || NotificationPreferenceFragment.class.getName().equals(fragmentName);
+               || AppearancePreferenceFragment.class.getName().equals(fragmentName)
+               || syncPreferenceFragment.class.getName().equals(fragmentName);
     }
 
     /**
@@ -209,55 +150,14 @@ public class SettingsActivity extends AppCompatPreferenceActivity
      * activity is showing a two-pane settings UI.
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class GeneralPreferenceFragment extends PreferenceFragment
+    public static class AppearancePreferenceFragment extends PreferenceFragment
     {
         @Override
         public void onCreate(Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.pref_general);
+            addPreferencesFromResource(R.xml.pref_appearance);
             setHasOptionsMenu(true);
-
-            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
-            // to their values. When their values change, their summaries are
-            // updated to reflect the new value, per the Android Design
-            // guidelines.
-            bindPreferenceSummaryToValue(findPreference("example_text"));
-            bindPreferenceSummaryToValue(findPreference("example_list"));
-        }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item)
-        {
-            int id = item.getItemId();
-            if (id == android.R.id.home)
-            {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
-    }
-
-    /**
-     * This fragment shows notification preferences only. It is used when the
-     * activity is showing a two-pane settings UI.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class NotificationPreferenceFragment extends PreferenceFragment
-    {
-        @Override
-        public void onCreate(Bundle savedInstanceState)
-        {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.pref_notification);
-            setHasOptionsMenu(true);
-
-            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
-            // to their values. When their values change, their summaries are
-            // updated to reflect the new value, per the Android Design
-            // guidelines.
-            bindPreferenceSummaryToValue(findPreference("notifications_new_message_ringtone"));
         }
 
         @Override
@@ -278,20 +178,19 @@ public class SettingsActivity extends AppCompatPreferenceActivity
      * activity is showing a two-pane settings UI.
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class DataSyncPreferenceFragment extends PreferenceFragment
+    public static class syncPreferenceFragment extends PreferenceFragment
     {
         @Override
         public void onCreate(Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.pref_data_sync);
+            addPreferencesFromResource(R.xml.pref_sync);
             setHasOptionsMenu(true);
+            Preference timeout = findPreference("timeout");
+            timeout.setOnPreferenceChangeListener(listener);
 
-            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
-            // to their values. When their values change, their summaries are
-            // updated to reflect the new value, per the Android Design
-            // guidelines.
-            bindPreferenceSummaryToValue(findPreference("sync_frequency"));
+            timeout.setSummary(String.format(
+                    getString(R.string.pref_title_timeout_value), prefs.getString("timeout", "0")));
         }
 
         @Override
